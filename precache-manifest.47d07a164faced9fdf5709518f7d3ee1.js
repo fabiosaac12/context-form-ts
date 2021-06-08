91bd45ca0893933fb244b62a27533382b10ef9f8
Fabio Isaac <fabiosaac12@gmail.com>
2021-06-07 21:52:39 -0400

    Updates

---
+++ precache-manifest.47d07a164faced9fdf5709518f7d3ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb36ca6173a87df3874a0bbc550bc80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b343e690a723a67cc25f",
+    "url": "./static/js/2.e168793a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e168793a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec080874d703471225a4",
+    "url": "./static/js/main.e240ec07.chunk.js"
+  },
+  {
+    "revision": "a1a1090b6421748b697d",
+    "url": "./static/js/runtime-main.e41b2a7e.js"
+  }
+]);
